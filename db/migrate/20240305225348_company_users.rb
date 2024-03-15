c0a9bc017dbea86d6a654d518bb9a8f49cd8f643
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-14 21:27:27 -0300

    Add a join table to company and users

---
+++ db/migrate/20240305225348_company_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CompanyUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :company_users do |t|
+      t.references :company, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
