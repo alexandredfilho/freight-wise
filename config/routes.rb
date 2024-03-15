7d24222a65f3f67ec108160ff220b3468da7c458
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-14 21:28:46 -0300

    Add companies routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
                        sessions: 'users/sessions',
                        registrations: 'users/registrations'
                      }
+  namespace :api do
+    namespace :v1 do
+      resources :companies
+    end
+  end
 end
