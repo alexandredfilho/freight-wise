a7d29fb6f6127564196aff9aa2ae70a5ea869007
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-14 21:36:45 -0300

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+User.create!(
+  email: 'admin@admin.com',
+  password: 'VerySecure@2024',
+  password_confirmation: 'VerySecure@2024',
+  company_attributes: {
+    name: 'My Company'
+  }
+)
