a81c27656990bc25a978ed19051c6aad41ac671b
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-18 20:07:29 -0300

    Add carrier serializer

---
+++ app/serializers/api/v1/carrier_serializer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+# frozen_string_literal: true
+
+module API
+  module V1
+    #
+    # Carrier serializer
+    #
+    class CarrierSerializer
+      include JSONAPI::Serializer
+      attributes :name, :company_id
+    end
+  end
+end
+
+# == Schema Information
+#
+# Table name: carriers
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :bigint           not null
+#
+# Indexes
+#
+#  index_carriers_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
