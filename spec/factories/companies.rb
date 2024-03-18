b26504a7c0558a6dd91a1d94271ad43428a6db44
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-18 20:13:52 -0300

    Add annotations to factories and unit tests

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -3,3 +3,13 @@ FactoryBot.define do
     name { "MyString" }
   end
 end
+
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
---
+++ spec/models/carrier_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Carrier, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# == Schema Information
+#
+# Table name: carriers
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :bigint           not null
+#
+# Indexes
+#
+#  index_carriers_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
