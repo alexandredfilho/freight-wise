e31096d599777a8f7e79847576cdd506fad30ee3
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-18 20:07:11 -0300

    Create carrier model

---
+++ app/models/carrier.rb
@@ -0,0 +1,22 @@
+class Carrier < ApplicationRecord
+  belongs_to :company
+end
+
+# == Schema Information
+#
+# Table name: carriers
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :bigint           not null
+#
+# Indexes
+#
+#  index_carriers_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
