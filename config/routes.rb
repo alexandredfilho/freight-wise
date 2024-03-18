1cadfbe5688571c5014ecf0deea52605bdf25a78
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-18 20:57:26 -0300

    Add freight charge routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'freight_charges/index'
   get 'current_user', to: 'users/current_user#index'
   devise_for :users, path: '', path_names: {
                                  sign_in: 'sign_in',
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :companies
       resources :carriers
+      resources :freight_charges
     end
   end
 end
