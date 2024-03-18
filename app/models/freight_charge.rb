c07df04a8503ab6109ce336267fc09a533261c03
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-18 20:56:34 -0300

    Create freight change model

---
+++ app/models/freight_charge.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+#
+# This model is responsible to create new instances of Freight Charges
+#
+class FreightCharge < ApplicationRecord
+  belongs_to :carrier
+end
+
+# == Schema Information
+#
+# Table name: freight_charges
+#
+#  id         :bigint           not null, primary key
+#  table      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  carrier_id :bigint           not null
+#
+# Indexes
+#
+#  index_freight_charges_on_carrier_id  (carrier_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (carrier_id => carriers.id)
+#
