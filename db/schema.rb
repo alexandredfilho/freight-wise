30a72033529299fe65dc9218bd5e6cc367de8a6c
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-18 20:11:29 -0300

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_12_230356) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_15_194428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carriers", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_carriers_on_company_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "company_users", force: :cascade do |t|
-    t.bigint "company_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_company_users_on_company_id"
-    t.index ["user_id"], name: "index_company_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_12_230356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "company_users", "companies"
-  add_foreign_key "company_users", "users"
+  add_foreign_key "carriers", "companies"
   add_foreign_key "users", "companies"
 end
