8ed2cfdb1a0053e04e3f87d4a2fd40fbc04945e9
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-19 23:04:09 -0300

    Add unit tests for city, state and customer models

---
+++ spec/models/city_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe City, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  state_id   :bigint           not null
+#
+# Indexes
+#
+#  index_cities_on_state_id  (state_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (state_id => states.id)
+#
---
+++ spec/models/state_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe State, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# == Schema Information
+#
+# Table name: states
+#
+#  id         :bigint           not null, primary key
+#  acronym    :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
