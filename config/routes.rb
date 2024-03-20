bdf212b42b6810211a29080cdd3a88f7647d78c8
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-19 23:02:52 -0300

    Fix routes and add customers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'freight_charges/index'
   get 'current_user', to: 'users/current_user#index'
+
   devise_for :users, path: '', path_names: {
                                  sign_in: 'sign_in',
                                  sign_out: 'sign_out',
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
                      }
   namespace :api do
     namespace :v1 do
-      resources :companies
       resources :carriers
+      resources :companies
+      resources :customers
       resources :freight_charges
     end
   end
