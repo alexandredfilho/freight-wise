dc9952f1f7bcbb53525c9a23cabcd19f7e958115
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-03-23 17:37:43 -0300

    Add new steps to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,25 @@ require 'tty-spinner'
 
 spinners = TTY::Spinner::Multi.new("[:spinner] Initializing database preparation!")
 
-sp1 = spinners.register "[:spinner] Creating a user and company..."
-sp1.auto_spin
+# Create a user and company with nested attributes
+create_user_and_company = spinners.register "[:spinner] Creating a user and company..."
+create_user_and_company.auto_spin
 
 User.create!(
-  email: 'admin@admin.com',
+  email: 'admin2@admin.com',
   password: 'VerySecure@2024',
   password_confirmation: 'VerySecure@2024',
   company_attributes: {
-    name: 'My Company'
+    name: Faker::Company.name
   }
 )
 
-sp1.success
+create_user_and_company.success
+# User and Company were created
 
-sp2 = spinners.register "[:spinner] Fetching states and cities from IBGE API..."
-sp2.auto_spin
+# Fetch states and cities from IBGE public API
+fetch_sates_and_cities = spinners.register "[:spinner] Fetching states and cities from IBGE API..."
+fetch_sates_and_cities.auto_spin
 
 states_and_cities = ::PublicAPI::IbgeService.execute
 
@@ -29,4 +32,51 @@ states_and_cities.each do |state_data|
   end
 end
 
-sp2.success
+fetch_sates_and_cities.success
+# States and cities were retrieved from the IBGE public API
+
+# Create customers and assign them to the first company
+create_customers = spinners.register "[:spinner] Creating Customers"
+create_customers.auto_spin
+
+5.times do
+  Customer.create!(
+    name: Faker::Company.name,
+    company_id: 1
+  )
+end
+
+create_customers.success
+# Customers were created
+
+# Create address and assign it to the Company
+create_company_address = spinners.register "[:spinner] Creating Company Address..."
+create_company_address.auto_spin
+
+Address.create(
+  street: Faker::Address.street_name,
+  street_details: Faker::Address.secondary_address,
+  city_id: 1,
+  state_id: 1,
+  zip_code: Faker::Address.zip_code,
+  addressable_id: 1,
+  addressable_type: "Company",
+)
+create_company_address.success
+# Company address has been created
+
+# Create address and assign it to the Company
+create_customer_address = spinners.register "[:spinner] Creating Customer Address..."
+create_customer_address.auto_spin
+
+Address.create(
+  street: Faker::Address.street_name,
+  street_details: Faker::Address.secondary_address,
+  city_id: 1,
+  state_id: 1,
+  zip_code: Faker::Address.zip_code,
+  addressable_id: 1,
+  addressable_type: "Customer",
+)
+create_customer_address.success
+# Company address has been created
